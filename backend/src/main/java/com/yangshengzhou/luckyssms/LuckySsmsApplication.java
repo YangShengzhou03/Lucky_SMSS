635d77e28b5be3e7e991c0772a123749269c2099
Yangshengzhou <3555844679@qq.com>
2025-07-12 22:27:01 +0800

    Refactoring project

---
+++ backend/src/main/java/com/yangshengzhou/luckyssms/LuckySsmsApplication.java
@@ -0,0 +1,11 @@
+package com.yangshengzhou.luckyssms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckySsmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LuckySsmsApplication.class, args);
+    }
+}

42ac2d8de79b20fef0b70fc0dc0883e2e301f371
Yangshengzhou <3555844679@qq.com>
2025-07-13 23:52:38 +0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,13 +5,24 @@ const routes = [
   {
     path: '/',
     name: 'App',
-    component: App
+    component: App,
+    meta: { title: '首页' }
   }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  } else {
+    document.title = 'Lucky_SMS';
+  }
+  next();
 });
 
-export default router;
+export default router

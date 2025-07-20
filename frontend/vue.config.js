5c48b64352651f6fb9ce72e3e2aa548379f136e0
Yangshengzhou <3555844679@qq.com>
2025-07-20 21:46:33 +0800

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-// vue.config.js
-module.exports = {
+  transpileDependencies: true,
+  devServer: {
+    port: 8080
+  },
   chainWebpack: (config) => {
     config.plugin('define').tap((args) => {
       args[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = true;
       return args;
     });
-  },
-};
+  }
+})
